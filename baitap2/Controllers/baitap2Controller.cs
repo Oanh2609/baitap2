13d16b4504a32722524bc1fcac05b02b0ea4bbc8
vukim2609 <vukim2609@outlook.com>
2021-08-24 16:54:39 +0700

    Add project files.

---
+++ baitap2/Controllers/baitap2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using baitap2.Models;
+
+namespace baitap2.Controllers
+{
+    public class baitap2Controller : Controller
+
+    {
+        Giaiphuongtrinh gpt = new Giaiphuongtrinh();
+        // GET: baitap2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTBT1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTBT1(double heSoa, double heSob)
+        {
+            double x = gpt.GiaiPhuongTrinhBacNhat(heSoa, heSob);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+
+    }
+}
---
+++ baitap2/Models/Giaiphuongtrinh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap2.Models
+{
+    public class Giaiphuongtrinh
+    {
+        public double GiaiPhuongTrinhBacNhat(double a, double b)
+        {
+        double x;
+            x = -b / a;
+            return x;
+
+
+
+    }
+    }
+}

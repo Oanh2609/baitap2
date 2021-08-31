3e6ec334ee3cf0a400025dc04413340e4b7f70bd
vukim2609 <vukim2609@outlook.com>
2021-08-31 13:44:28 +0700

    B

--- baitap2/Models/Giaiphuongtrinhbacnhat.cs
+++ baitap2/Models/Giaiphuongtrinhbacnhat.cs
@@ -7,6 +7,12 @@ namespace baitap2.Models
 {
     public class Giaiphuongtrinhbacnhat
     {
-
-    }
+       
+            public double GiaiPTBac1(double a, double b)
+            {
+                double x;
+                x = -b / a;
+                return x;
+            }
+        }
 }
